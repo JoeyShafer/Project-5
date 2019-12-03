8f819d5cd4af9964a45b473d1a2195094b184666
Joey Shafer <Joseph.m.shafer-1@ou.edu>
2019-12-02 23:11:51 -0600

    added action for the show stations button

--- Main.java
+++ Main.java
@@ -4,9 +4,13 @@ package application;
 
 import java.util.ArrayList;
 
+
+
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -36,7 +40,7 @@ public class Main extends Application {
 			TextField hammingDistField = new TextField();
 			
 			// create hamming distance slider
-			Slider slider = new Slider(1,4,2);
+			Slider slider = new Slider(1,4,1);
 			slider.setMajorTickUnit(1);
 			slider.setMinorTickCount(0);
 			slider.setSnapToTicks(true);
@@ -56,6 +60,7 @@ public class Main extends Application {
 			for (String s : stationList) {
 				stations.getItems().add(s);
 			}
+			
 			/*
 			 * When the slider is moved the value hammingDist will be changed to value 
 			 * if the slider and then the hammingDistField will show the value of the 
@@ -68,6 +73,27 @@ public class Main extends Application {
 		        	 hammingDist = (int)slider.getValue();
 		        	 hammingDistField.setText(Integer.toString(hammingDist));
 		         }
+			});
+			
+			showStation.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				 public void handle(ActionEvent event) {
+					// Get the stid from the ChoiceBox
+					String stid = (String)stations.getValue();
+					
+					// Get the List of Stations that share the same Hamming distance 
+					// with the stid
+					ArrayList<String> stationsWithHammDist = 
+							hammDistance.stationsWithHammingDist(stid, hammingDist);
+					
+					// Clear all the text from the listView of the stations
+					listStations.getItems().clear();
+					// Add all the stations that share the same Hamming distance 
+					// to the ListView
+					for (String s : stationsWithHammDist) {
+						listStations.getItems().add(s);
+					}
+				}
 			});
 			
 			
